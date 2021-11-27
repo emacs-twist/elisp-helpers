92cb619d90ca45bc8f8158bf4bc904e4ccc24d5b
Akira Komamura <akira.komamura@gmail.com>
2021-11-28 04:06:11 +0900

    style: Rewrite parseCask

--- lib/utils.nix
+++ lib/utils.nix
@@ -2,20 +2,6 @@
 { lib }:
 with builtins;
 let
-  # Look up a key in an alist-like structure.
-  lookup = key: xs:
-    let
-      found = filter (x: head x == key) xs;
-    in
-    if length found > 0 then tail (head found) else null;
-
-  # Filter sublists whose head equals to key and return their tails.
-  select = key: xs:
-    if xs == null then [ ] else map tail (filter (x: head x == key) xs);
-
-  # If a non-empty list is given, return its head. Otherwise return null.
-  safeHead = xs: if isList xs && length xs > 0 then head xs else null;
-
   # Transform a flat list into a nested list
   plistToAlist = xs:
     if length xs == 0 then
@@ -41,5 +27,5 @@ let
     ];
 in
 {
-  inherit lookup select safeHead plistToAlist alistToAttrs;
+  inherit plistToAlist alistToAttrs;
 }
