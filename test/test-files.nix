071b22a2d323ab929ebc5bcf3516b581d023c7bf
Akira Komamura <akira.komamura@gmail.com>
2022-01-12 22:41:36 +0900

    feat!: Return an attribute set from lib/expandMelpaRecipeFiles

--- test/test-files.nix
+++ test/test-files.nix
@@ -12,19 +12,19 @@ in
 pkgs.lib.runTests {
   testExpandResult = {
     expr = expandMelpaRecipeFiles ./. defaultFilesSpec;
-    expected = [
-      "hello.el"
-      "hello2.el"
-      "doc/hello.info"
-    ];
+    expected = {
+      "hello.el" = "hello.el";
+      "hello2.el" = "hello2.el";
+      "doc/hello.info" = "hello.info";
+    };
   };
 
   testNestedExpansion1 = {
     expr = expandMelpaRecipeFiles ./nested1 recipe3.files;
-    expected = [
-      "flymake-perlcritic.el"
-      "bin/flymake_perlcritic"
-    ];
+    expected = {
+      "flymake-perlcritic.el" = "flymake-perlcritic.el";
+      "bin/flymake_perlcritic" = "bin/flymake_perlcritic";
+    };
   };
 
   testNestedExpansion2 = {
@@ -37,12 +37,12 @@ pkgs.lib.runTests {
           [ "python-mode" "snippets/python-mode/*" ]
         ]
       ];
-    expected = [
-      "snippets/html-mode/1"
-      "snippets/html-mode/2"
-      "snippets/python-mode/def"
-      "snippets/python-mode/while"
-    ];
+    expected = {
+      "snippets/html-mode/1" = "snippets/html-mode/1";
+      "snippets/html-mode/2" = "snippets/html-mode/2";
+      "snippets/python-mode/def" = "snippets/python-mode/def";
+      "snippets/python-mode/while" = "snippets/python-mode/while";
+    };
   };
 
   testNestedExpansion3 = {
@@ -51,18 +51,18 @@ pkgs.lib.runTests {
         "src/*.el"
         "snippets"
       ];
-    expected = [
-      "src/pony-mode.el"
-      "src/pony-tpl.el"
-      "snippets"
-    ];
+    expected = {
+      "src/pony-mode.el" = "pony-mode.el";
+      "src/pony-tpl.el" = "pony-tpl.el";
+      "snippets" = "snippets";
+    };
   };
 
   testNestedExpansion4 = {
     expr = expandMelpaRecipeFiles ./org-starter recipe4.files;
-    expected = [
-      "org-starter-utils.el"
-      "org-starter.el"
-    ];
+    expected = {
+      "org-starter-utils.el" = "org-starter-utils.el";
+      "org-starter.el" = "org-starter.el";
+    };
   };
 }
